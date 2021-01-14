1dd418c8ae83ee2701e30ca86bcec44c74d4ffb5
bepis <luqdek@gmail.com>
2021-01-14 15:16:31 +0000

    Ensured that the draw function would not overwrite an occupied space

--- script.rb
+++ script.rb
@@ -2,7 +2,13 @@ class Board
   def initialize
     @map = [[], [], []]
   end
-  def draw(row,column)
-    #need to ensure the position being drawn on can't be occupied
+
+  def draw(row, column,drawing)
+    # need to ensure the position being drawn on can't be occupied
+    if @map[row][column].empty?
+      @map[row][column] = drawing
+    else
+      puts "That spot is occupied, please try again"
+    end
   end
 end
