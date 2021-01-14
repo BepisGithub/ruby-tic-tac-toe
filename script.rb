051b6eae723d647f0212c2bc24216d0fb2cc8c54
bepis <luqdek@gmail.com>
2021-01-14 15:37:53 +0000

    Added a variable to the player to indicate whether they are the active player

--- script.rb
+++ script.rb
@@ -20,6 +20,7 @@ class Player
   def initialize(name)
     @name = name
     @has_won = false
+    @active_player = false
     (@@number_of_players % 2).zero? ? @drawing = "X" : @drawing = "O"
     @@number_of_players += 1
   end
