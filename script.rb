e7cdd2c651dab376750f9247936719e0ed4bcfeb
bepis <luqdek@gmail.com>
2021-01-15 11:49:07 +0000

    Added functionality for the #v_winner? method

--- script.rb
+++ script.rb
@@ -80,11 +80,14 @@ class Game
     return true if hash["top"][0] == hash["top"][1] && hash["top"][0] == hash["top"][2]
     return true if hash["mid"][0] == hash["mid"][1] && hash["mid"][0] == hash["mid"][2]
     return true if hash["bot"][0] == hash["bot"][1] && hash["bot"][0] == hash["bot"][2]
+
     false
   end
 
   def v_winner? hash
-
+    3.times do |i|
+      return true if hash["top"][i] == hash["mid"][i] && hash["mid"][i] == hash["bot"][i]
+    end
   end
 
   def d_winner? hash
